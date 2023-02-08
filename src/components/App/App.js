4e24adbbc3f819e11924ae6032b6c9fdd660fbe9
emilytien <emilytien421@gmail.com>
2023-02-08 16:22:03 +0800

    修正換頁後位置不在top of the window

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,24 +8,26 @@ import Posts from "../Blog/Posts";
 import ContactMe from "../ContactMe";
 import NotFound from "../NotFound";
 import Footer from "../Footer";
+import ScrollToTop from "../ScrollToTop";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
-      <Header />
-
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route exact path="/AboutFei" element={<AboutFei />} />
-        <Route exact path="/ClassFeature" element={<ClassFeature />} />
-        <Route exact path="/OnlineClass" element={<OnlineClass />} />
-        <Route exact path="/Blog" element={<Blog />} />
-        <Route exact path="/Blog/:id" element={<Posts />} />
-        <Route exact path="/ContactMe" element={<ContactMe />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+      <ScrollToTop>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/AboutFei" element={<AboutFei />} />
+          <Route exact path="/ClassFeature" element={<ClassFeature />} />
+          <Route exact path="/OnlineClass" element={<OnlineClass />} />
+          <Route exact path="/Blog" element={<Blog />} />
+          <Route exact path="/Blog/:id" element={<Posts />} />
+          <Route exact path="/ContactMe" element={<ContactMe />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </ScrollToTop>
     </HashRouter>
   );
 }
